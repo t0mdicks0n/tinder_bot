b9566ac6fcfa96c6919754b4d10a4370013eb092
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-08-23 08:18:30 +0200

    Seem to not genereate any more users..

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import pynder
 
-fb_id = "522777200"
-fb_token = 'EAAGm0PX4ZCpsBAKdh3yhvwsnyTQj1RBDEcpgxxZACejSS8n3KZAkbTtvW1by9jQBMZAVvZArgKK21d2Grfe4js8CFWwSyA0ZAV2X7BOyNNRmjdMbkZCZCfTJdE7W07ZByqxEhNqZCoZAhLwTZAElSSAawIjZAGlxpLPmtUDIxnYP4V9y5sHKgpSWtuSdxZAhHrYHCNTyhKIX0nOvT7Ey8AFtFav1YsT0YP50vFb2EZAD1dDiwdYr7i0doivmWAWUMxD8rbTtA8I67HDCcGzyQZDZD'
+# Find facebook token: https://gist.github.com/rtt/10403467#gistcomment-1846343
+
+fb_id = ""
+fb_token = ''
+
+num_liked = 0
 
 session = pynder.Session(facebook_id=fb_id, facebook_token=fb_token)
 users = session.nearby_users()
 
 for user in users :
-	print(user)
 	user.like()
+	print("Just liked ", user, ", I have no liked ", str(num_liked), " babes in total.")
+	num_liked += 1
 
+# def like_users() :
+# 	for user in users :
+# 		print("Starting over")
+# 		try : 
+# 			user.like()
+# 			print("Just liked ", user, ", I have no liked ", str(num_liked), " babes in total.")
+# 			num_liked += 1
+# 		except :
+# 			print("I crashed: ")
+# 			like_users()
+
+
+# like_users()
